1d73d9d4ace02d9d40c5735a52f05528bf33441a
DK32093 <107143112+DK32093@users.noreply.github.com>
2024-11-19 14:35:37 -0500

    Add street trees layer from geoserver

--- javascript.js
+++ javascript.js
@@ -1,6 +1,14 @@
-const map = L.map('map').setView([51.505, -0.09], 13);
+const map = L.map('map').setView([42.3601, -71.0589], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+//load street trees layer from geoserver
+const street_trees = L.tileLayer.wms("http://localhost:8080/geoserver/Trees_BOS/wms", {
+    layers: 'Trees_BOS:treekeeper_street_trees',
+    format: 'image/png',
+    transparent: true,
+});
+street_trees.addTo(map);
