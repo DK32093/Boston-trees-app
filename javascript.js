32af886c072fe9d8ad4e9b89d6713964b5b1037f
DK32093 <107143112+DK32093@users.noreply.github.com>
2024-11-19 16:29:04 -0500

    Add Boston border via url

--- javascript.js
+++ javascript.js
@@ -5,10 +5,24 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
+let url = 'https://data.boston.gov/dataset/a70595d2-fd38-4bcb-8a81-6f7807621d38/resource/dade0744-a486-44c7-be7d-07240a89dca4/download/city_of_boston_outline_boundary_water_excluded.geojson';
+const response = fetch(url).then(response => response.json()).then(response => {
+      L.geoJson(response, {
+        style: { 
+            "color": "#000000",
+            "fillColor": "none",
+            "weight": 2,
+            "opacity": 1
+        }
+      }
+    ).addTo(map)
+});
+
 //load street trees layer from geoserver
 const street_trees = L.tileLayer.wms("http://localhost:8080/geoserver/Trees_BOS/wms", {
     layers: 'Trees_BOS:treekeeper_street_trees',
     format: 'image/png',
     transparent: true,
 });
+
 street_trees.addTo(map);
