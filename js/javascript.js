1d5ffa1d6c59acee19bae1d09398caed876fc7ee
DK32093 <107143112+DK32093@users.noreply.github.com>
2024-11-20 22:19:42 -0500

    Set initial popup on click

--- js/javascript.js
+++ js/javascript.js
@@ -1,12 +1,15 @@
-const map = L.map('map').setView([42.3601, -71.0589], 13);
+const map = L.map('map').setView([42.31000, -71.064881], 11.5);
 
+// Get OSM basemap
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
+    maxZoom: 20,
+    maxNativeZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
+// Get Boston border GeoJSON and add style
 let url = 'https://data.boston.gov/dataset/a70595d2-fd38-4bcb-8a81-6f7807621d38/resource/dade0744-a486-44c7-be7d-07240a89dca4/download/city_of_boston_outline_boundary_water_excluded.geojson';
-const response = fetch(url).then(response => response.json()).then(response => {
+let response = fetch(url).then(response => response.json()).then(response => {
       L.geoJson(response, {
         style: { 
             "color": "#000000",
@@ -16,13 +19,17 @@ const response = fetch(url).then(response => response.json()).then(response => {
         }
       }
     ).addTo(map)
-});
+}); 
 
 //load street trees layer from geoserver
-const street_trees = L.tileLayer.wms("http://localhost:8080/geoserver/Trees_BOS/wms", {
+const street_trees = L.tileLayer.betterWms("http://localhost:8080/geoserver/Trees_BOS/wms", {
+    maxZoom: 20,
     layers: 'Trees_BOS:treekeeper_street_trees',
     format: 'image/png',
-    transparent: true,
+    transparent: true
 });
 
 street_trees.addTo(map);
+
+
+
