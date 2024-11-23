742229931435ec8bb6870825d4a8c4b3be87f45b
DK32093 <107143112+DK32093@users.noreply.github.com>
2024-11-22 19:25:29 -0500

    Update cql filter and add browser icon

--- js/javascript.js
+++ js/javascript.js
@@ -26,7 +26,7 @@ const street_trees = L.tileLayer.betterWms("http://localhost:8080/geoserver/Tree
     layers: 'Trees_BOS:treekeeper_street_trees',
     format: 'image/png',
     transparent: true,
-    cql_filter: "full_name<>'Empty Pit/Planting Site' AND full_name<>'Vacant Unacceptable/Retired' AND full_name<>'Scheduled Planting Site - Fall 2024'"
+    cql_filter: "full_name <> 'Empty Pit/Planting Site'"
 }).addTo(map);
 
 // Create legend
