1853068f04832552195fe90b7ed84535bf0f5784
DK32093 <107143112+DK32093@users.noreply.github.com>
2024-11-25 17:09:17 -0500

    Update area border and variable names

--- js/javascript.js
+++ js/javascript.js
@@ -1,5 +1,5 @@
 
-const map = L.map('map').setView([42.31000, -71.064881], 11.5);
+const map = L.map('map').setView([42.34488628077827, -71.06897719272507], 15);
 
 // Get basemap
 const Esri_WorldStreetMap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
@@ -8,8 +8,8 @@ const Esri_WorldStreetMap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/
     maxNativeZoom: 19
 }).addTo(map);
 
-// Get Boston border GeoJSON and add style
-const url = 'https://data.boston.gov/dataset/a70595d2-fd38-4bcb-8a81-6f7807621d38/resource/dade0744-a486-44c7-be7d-07240a89dca4/download/city_of_boston_outline_boundary_water_excluded.geojson';
+// Get Back Bay border GeoJSON and add style
+const url = 'geo/BackBay.geojson';
 const response = fetch(url).then(response => response.json()).then(response => {
       L.geoJson(response, {
         style: { 
@@ -31,7 +31,7 @@ function siteFilter(feature) {
 };
 
 //Initial point style
-var geojsonMarkerOptions = {
+const treeStyle1 = {
     radius: 3,
     fillColor: "#544a24",
     color: "#2A7E19",
@@ -41,7 +41,7 @@ var geojsonMarkerOptions = {
 };
 
 //Zoomed-in point style
-var geojsonMarkerOptions2 = {
+const treeStyle2 = {
     radius: 12,
     fillColor: "#544a24",
     color: "#2A7E19",
@@ -56,14 +56,14 @@ const ST_response = fetch(ST_url).then(response => response.json()).then(ST_resp
       treeGeoJSON = L.geoJson(ST_response, {
         filter: siteFilter,
         pointToLayer: function (feature, latlng) {
-            full_name = feature.properties.spp_com.split(',').reverse().join(" ");
-            let marker = L.circleMarker(latlng, geojsonMarkerOptions);
+            let full_name = feature.properties.spp_com.split(',').reverse().join(" ");
+            let marker = L.circleMarker(latlng, treeStyle1);
             return marker.bindPopup(full_name)
         }
       }).addTo(map)
 }); 
 
-//Change styling a max zoom and reset when zooming back out
+//Change styling at max zoom and reset when zooming back out
 let c = 0
 map.on('zoomend', function() {
     const currentZoom = map.getZoom();
@@ -73,8 +73,8 @@ map.on('zoomend', function() {
             treeGeoJSONz = L.geoJson(ST_response, {
               filter: siteFilter,
               pointToLayer: function (feature, latlng) {
-                full_name = feature.properties.spp_com.split(',').reverse().join(" ");
-                let marker = L.circleMarker(latlng, geojsonMarkerOptions2);
+                let full_name = feature.properties.spp_com.split(',').reverse().join(" ");
+                let marker = L.circleMarker(latlng, treeStyle2);
                 return marker.bindPopup(full_name);
               }
             }).addTo(map)
@@ -87,8 +87,8 @@ map.on('zoomend', function() {
                 treeGeoJSON = L.geoJson(ST_response, {
                   filter: siteFilter,
                   pointToLayer: function (feature, latlng) {
-                    full_name = feature.properties.spp_com.split(',').reverse().join(" ");
-                    let marker = L.circleMarker(latlng, geojsonMarkerOptions);
+                    let full_name = feature.properties.spp_com.split(',').reverse().join(" ");
+                    let marker = L.circleMarker(latlng, treeStyle1);
                     return marker.bindPopup(full_name);
                   }
                 }).addTo(map)
@@ -96,29 +96,28 @@ map.on('zoomend', function() {
         }
         return c = 0
     }
-})
-
+});
 
 // Create legend
-var legend = L.control({position: 'bottomright'});
+const legend = L.control({position: 'bottomright'});
 legend.onAdd = function (map) {
-    var div = L.DomUtil.create('div', 'info');
+    const div = L.DomUtil.create('div', 'info');
     div.innerHTML += '<img src="assets/simple_tree.png" width=40 height=40 alt="street tree"><p>Street Tree</p>';
     return div;
 };
 
 // Create logo
-var logo = L.control({position: 'bottomleft'});
+const logo = L.control({position: 'bottomleft'});
 logo.onAdd = function (map) {
-    var div = L.DomUtil.create('div', 'logo');
-    var pic = L.DomUtil.create('div', 'pic', div)
-    var title = L.DomUtil.create('div', 'title', div);
-    title.innerHTML += '<p style=font-size:2.5rem;font-weight:bold>What&#39s That Tree?</p><p style=font-size:1.5rem> Street Tree Species in Boston, MA</p>';
+    const div = L.DomUtil.create('div', 'logo');
+    const pic = L.DomUtil.create('div', 'pic', div)
+    const title = L.DomUtil.create('div', 'title', div);
+    title.innerHTML += '<p style=font-size:2.5rem;font-weight:bold>What&#39s That Tree?</p><p style=font-size:1.5rem> Street Tree Species in Boston, MA</p><p><em>Back Bay Demo</em></p>';
     pic.innerHTML += '<img src="assets/dk.png" width=70 height=70 alt="Dylan Kelly"><p><strong>Dylan Kelly</strong></p><a href="https://github.com/DK32093/Boston-trees-app/tree/main" style=color:white>Github</a>';
     return div;
 };
 
+//Add legend, logo, and geocoder
 legend.addTo(map);
 logo.addTo(map);
-
 L.Control.geocoder().addTo(map);
